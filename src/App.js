63716baa01676a466da3ff23977a8b1e3736fb41
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-04-30 20:32:29 +0530

    NavBar v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-
-      {/* Navbar */}
+      <Nav />
 
       {/* App */}
 
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <div>Logo</div>
+
+      <div>ChangeLog</div>
+
+      <div>DL</div>
+    </div>
+  );
+}
+
+export default Nav;
