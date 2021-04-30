4bc5aa979a7697ded9aabfb0eb1f09d8ed3ebf84
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-01 02:13:15 +0530

    same

--- src/Weather.js
+++ src/Weather.js
@@ -4,12 +4,13 @@ import axios from "axios";
 
 function Weather() {
   const API_KEY = "83ae873f99e8cca64fd2e00d659b24be";
-  const [CityName, setCityName] = useState("London");
+  const [CityName, setCityName] = useState("Gwalior");
   const [Id, setId] = useState();
   const [Min, setMin] = useState();
   const [Max, setMax] = useState();
   const [CurrentTemp, setCurrentTemp] = useState();
   const [MainWeather, setMainWeather] = useState();
+  const [Humi, setHumi] = useState();
 
   const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${CityName}&units=metric&appid=${API_KEY}`;
 
@@ -21,7 +22,7 @@ function Weather() {
       setMax(result.data.main.temp_max);
       setMainWeather(result.data.weather[0].main);
       //   setDesc(result.data.weather[0].description);
-      //   setHumi(result.data.main.humidity);
+      setHumi(result.data.main.humidity);
       setCurrentTemp(result.data.main.temp);
       //   setIcon(result.data.weather[0].icon);
     }
@@ -35,13 +36,6 @@ function Weather() {
 
   return (
     <div className="weather__component">
-      {/* <input
-        value={CityName}
-        className="search"
-        onChange={(e) => setCityName(e.target.value)}
-      ></input>
-      <h1 className="city__name">Hello</h1>
-      <h1 className="weather__image">Image</h1> */}
       <input
         className="search"
         placeholder="City Name"
@@ -57,8 +51,8 @@ function Weather() {
         <h3>Maximum: {Max} °C</h3>
       </div>
       <div className="wind">
-        <h3>Wind Speed:</h3>
-        <h3>Wind Direction</h3>
+        <h3>Humidity: {Humi}</h3>
+        <h3>Wind Speed</h3>
       </div>
     </div>
   );
