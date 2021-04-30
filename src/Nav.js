2b2c836cd2ef9f73f88c520cd4a7499d34c758d8
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-04-30 20:34:40 +0530

    NavBar v3

--- src/Nav.js
+++ src/Nav.js
@@ -2,12 +2,15 @@ import React from "react";
 
 function Nav() {
   return (
-    <div>
-      <div>Logo</div>
+    <div className="nav__main">
+      <div className="nav__left">
+        <p>Logo</p>
+      </div>
 
-      <div>ChangeLog</div>
-
-      <div>DL</div>
+      <div className="nav__right">
+        <p>Changelog</p>
+        <p>DL</p>
+      </div>
     </div>
   );
 }
