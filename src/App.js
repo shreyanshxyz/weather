89ec9503a670992c6e3cefe928971cd4a4b5115c
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-01 03:07:07 +0530

    navigation bar re added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-// import Nav from "./Nav";
+import Nav from "./Nav";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      {/* <Nav /> */}
+      <Nav />
 
       <Weather />
 
