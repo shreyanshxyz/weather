e793a8a60b4662c9addeeefaabd5221db01cbe60
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-01 18:47:14 +0530

    Navbar Removed for Now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import Nav from "./Nav";
+// import Nav from "./Nav";
 import Weather from "./Weather";
 import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      {/* <Nav /> */}
 
       <Weather />
 
